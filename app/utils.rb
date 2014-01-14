8ff71ad8620c19009b9208cada41e6fc85bb549c
raymoo <notanemail@fakeaddress.net>
2014-01-13 19:06:11 -0800

    Actually there was still an uppercase json

--- app/utils.rb
+++ app/utils.rb
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-require 'JSON'
+require 'json'
 
 module CBUtils
   def self.condense_name name
