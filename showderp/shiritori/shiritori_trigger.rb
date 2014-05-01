4793c3ae52261a3e427bb842f4c2a7555e9ae620
raymoo <notanemail@fakeaddress.net>
2014-04-30 20:13:30 -0700

    shiritori basically done, also better ordering of triggers

--- showderp/shiritori/shiritori_trigger.rb
+++ showderp/shiritori/shiritori_trigger.rb
@@ -1,3 +1,8 @@
+def just_latin?(str)
+  !!str.match(/^[a-zA-Z0-9_\-+ ]*$/)
+end
+
+
 Trigger.new do |t|
 
   t[:id] = 'shiritori'
@@ -9,16 +14,16 @@ Trigger.new do |t|
     info[:what][5..-1].strip
   }
   
-  t[:players] = Array.new
-  t[:used] = Array.new
-  t[:current] = 0
+  players = Array.new
+  used = Array.new
+  current = 0
   game = false
 
   t.act { |info|
     
     args = info[:result].split(' ')
     if args.count < 1 then
-      (t[:players].count > 0 && playerlist = "" + t[:players].join(', ') + ' **Current:**  ' + t[:players][t[:current]]) || playerlist = 'None'
+      (players.count > 0 && playerlist = "" + players.join(', ') + ' **Current:**  ' + players[current]) || playerlist = 'None'
       (game && !info[:respond].call('Players: ' + playerlist)) || info[:respond].call('There is currently no game')
       next
     end
@@ -32,23 +37,25 @@ Trigger.new do |t|
     speaker =  CBUtils.condense_name(info[:who])
     
     if args[0] == 'join' && game then
-      if t[:players].index speaker then
+      if players.index speaker then
         info[:respond].call("You are already in the game")
       else
-        t[:players].push(speaker)
-        (t[:players].count > 0 && playerlist = "" + t[:players].join(', ') + ' **Current:**  ' + t[:players][t[:current]]) || playerlist = 'None'
-        info[:respond].call('Players' + playerlist)
+        players.push(speaker)
+        (players.count > 0 && playerlist = "" + players.join(', ') + ' **Current:**  ' + players[current]) || playerlist = 'None'
+        info[:respond].call('Players: ' + playerlist)
       end
       next
     end
     
     if args[0] == 'leave' && game then
-      speakindex = t[:players].index speaker
+      speakindex = players.index speaker
       if speakindex then
-        t[:players].index speaker && t[:players].delete(speaker)
-        (t[:players].count > 0 && playerlist = "" + t[:players].join(', ') + ' **Current:**  ' + t[:players][t[:current]]) || playerlist = 'None'
-        info[:respond].call('Players' + playerlist)
-        speakindex <= t[:current] && t[:current] != 0 && t[:current]-= 1
+        players.index speaker && players.delete(speaker)
+        if speakindex == current && current != 0 then current %= 0 end
+        if speakindex < current then current -= 1 end
+
+        (players.count > 0 && playerlist = "" + players.join(', ') + ' **Current:**  ' + players[current]) || playerlist = 'None'
+        info[:respond].call('Players: ' + playerlist)
       else
         info[:respond].call("You're not in the game.")
       end
@@ -57,12 +64,35 @@ Trigger.new do |t|
     
     if args[0] == 'end' && game && (info[:all][2][0] == '#' || info[:all][2][0] == '@') then
       game = false
-      t[:players] = Array.new
-      t[:used] = Array.new
-      t[:current] = 0
+      players = Array.new
+      used = Array.new
+      current = 0
       info[:respond].call("Game Ended.")
       next
     end
     
+    if args[0] == 'play' && game then
+      prevword = used[-1]
+      if !(players.index speaker) then
+        info[:respond].call("You are not in the game.")
+      elsif players[current] != speaker
+        info[:respond].call("It is not your turn.")
+      elsif args.count < 2 || !just_latin?(args[1]) then
+        info[:respond].call("Please use a single latin-alphabet word")
+      elsif prevword && args[1].downcase[0] != prevword[-1]
+        info[:respond].call("That word doesn't fit. The previous word was #{prevword}.")
+      elsif used.index args[1].downcase
+        info[:respond].call("That word has been used")
+      else
+        used.push(args[1].downcase)
+        current = (current + 1) % players.count
+        prevword = used[-1]
+        info[:respond].call("#{players[current]}, your letter is **#{prevword[-1]}**.")
+      end
+      
+      next
+    end
+     
+
   }
 end
